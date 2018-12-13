8ae800d8c8b7eb1581933deea7cac6b0dd2aade5
Kevin Westropp <kwestropp@kevins-mbp.lan>
2018-12-13 00:34:31 -0600

    cleaning up profile component

--- src/profile/profile.ts
+++ src/profile/profile.ts
@@ -1,69 +1,52 @@
 import { LitElement, html, property } from '@polymer/lit-element';
-import { TemplateResult } from 'lit-html';
+// import { TemplateResult } from 'lit-html';
 // import { property } from '@polymer/decorators';
 
 export class ProfileComponent extends LitElement {
 
     @property({type: String})
+    public name: string = '';
+
+    @property({type: String})
+    public title: string = '';
+
+    @property({type: String})
     public url: string = '';
 
-    public render(): TemplateResult {
+    public render() {
         return html`
             <style>
-            ${this.styles}
+                .profile {
+                    display: flex;
+                    padding: 10px;
+                    text-align: center;
+                    line-height: 2px;
+                }
+                .profile.profile-container {
+                    flex-direction: column;
+                    flex-wrap: wrap;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .profile-name {
+                    font-weight: 400;
+                }
+                .profile-title {
+                    font-weight: 300;
+                    margin: 0;
+                }
+                .profile-pic {
+                    width: 75px;
+                    height: 75px;
+                    border: 0;
+                    border-radius: 50%;
+                }
             </style>
-            ${this.template}
-        `;
-    }
-
-    private template(): TemplateResult {
-        return html`
-        <div class="profile profile-container">
-            <div >
+            <div class="profile profile-container">
                 <img src="{{data?.picture}} class="profile-pic">
+                <h3 class="profile-name">${this.name}</h3>
+                <h5 class="profile-title">${this.title}</h5>
             </div>
-            <div class="profile-name-title">
-                <h3>{{name}}</h3>
-                <h5>{{title}}</h5>
-            </div>
-        </div>
-        `;
-    }
-
-    private styles(): TemplateResult {
-        return html`
-        .profile {
-            display: flex;
-            padding: 10px;
-        }
-
-        .profile.profile-container {
-              flex-direction: column;
-              flex-wrap: wrap;
-              justify-content: center;
-              align-items: center;
-              }
-              .profile-name-title {
-                text-align: center;
-                line-height: 2px;
-                h3 {
-                  font-weight: 400;
-                }
-                h5 {
-                  font-weight: 300;
-                  margin: 0;
-                }
-              }
-            }
-          }
-
-          .profile-pic {
-            img {
-              width: 75px;
-              height: 75px;
-              border: 0;
-              border-radius: 50%;
-            }
         `;
     }
 
