c2f6f581c4c4b00fedf0bdb51f44f25689fc9e41
Kevin.Westropp <kevinwestropp@live.com>
2019-04-02 11:45:29 -0500

    update to use litcss

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, customElement, property } from 'lit-element';
+import { LitElement, html, css, customElement, property } from 'lit-element';
 
 @customElement('kpw-profile')
 export class ProfileComponent extends LitElement {
@@ -11,37 +11,40 @@ export class ProfileComponent extends LitElement {
   @property({ type: String })
   public url: string = '';
 
+  static get styles() {
+    return css`
+      .profile {
+        display: flex;
+        padding: 10px;
+        text-align: center;
+        line-height: 2px;
+      }
+      .profile.profile-container {
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+      }
+      .profile-name {
+        font-weight: 400;
+      }
+      .profile-title {
+        font-weight: 300;
+        margin: 0;
+      }
+      .profile-pic {
+        width: 75px;
+        height: 75px;
+        border: 0;
+        border-radius: 50%;
+      }
+    `;
+  }
+
   public render() {
     return html`
-      <style>
-        .profile {
-          display: flex;
-          padding: 10px;
-          text-align: center;
-          line-height: 2px;
-        }
-        .profile.profile-container {
-          flex-direction: column;
-          flex-wrap: wrap;
-          justify-content: center;
-          align-items: center;
-        }
-        .profile-name {
-          font-weight: 400;
-        }
-        .profile-title {
-          font-weight: 300;
-          margin: 0;
-        }
-        .profile-pic {
-          width: 75px;
-          height: 75px;
-          border: 0;
-          border-radius: 50%;
-        }
-      </style>
       <div class="profile profile-container">
-        <img src="{{data?.picture}} class="profile-pic">
+        <img src="${this.url}" class="profile-pic" />
         <h3 class="profile-name">${this.name}</h3>
         <h5 class="profile-title">${this.title}</h5>
       </div>
