1204204405bf15fd89203bc3f4f638128f8bdb55
Kevin Westropp <kevinwestropp@gmail.com>
2020-01-09 16:16:45 -0700

    delete old stuff

--- src/components/header/index.ts
+++
@@ -1,9 +0,0 @@
-import { LitElement, html, customElement, property } from 'lit-element';
-import { TemplateResult } from 'lit-html';
-
-@customElement('kpw-header')
-export class HeaderComponent extends LitElement {
-  public render(): TemplateResult {
-    return html``;
-  }
-}
